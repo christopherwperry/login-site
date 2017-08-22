a95726eb7b39fbac6b127b464cb5711134c9c06e
christopherwperry <christopherwperry@gmail.com>
2017-08-21 22:23:54 -0400

    attempt add of req.flash

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const mustache = require('mustache-express');
 const session = require('express-session')
 const bodyParser = require('body-parser');
 const data = require('./users.js')
+const flash = require('req-flash');
 const port = 3000;
 const path = require('path');
 const app = express();
+
 
 app.engine('mustache', mustache());
 app.set('views', ['./views']);
@@ -14,20 +16,29 @@ app.set('view engine', 'mustache');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true
 }));
+app.use(flash())
 
 function checkUser(req, username, password){
   let authUser = data.users.find(function(user){
     if (username === user.username && password === user.password) {
-      req.session.authenticated = true;
+      req.session.username = true;
+      req.session.password = true;
       console.log("User & Password Authenticated");
+    // } else if (username !== user.username && password === user.password) {
+    //   req.session.username = false;
+    //   req.session.password = true;
+    // } else if (username === user.username && password !== user.password){
+    //   req.session.username = true;
+    //   req.session.password = false;
     } else {
-      return false;
+      return false
+      // req.session.username = false;
+      // req.session.password = false;
     };
   });
   console.log(req.session);
@@ -38,13 +49,20 @@ app.get('/', function(req,res){
   res.render('login');
 });
 
+app.get('/test', function(req,res) {
+    req.flash('successMessage', 'You are successfully using req-flash');
+    req.flash('errorMessage', 'No errors, you\'re doing fine');
+    res.redirect('/');
+});
+
 app.post("/", function (req, res) {
   let username = req.body.username;
   let password = req.body.password;
   checkUser(req, username, password);
-  if(req.session && req.session.authenticated){
+  if(req.session.username && req.session.password){
     res.render('index', {username: username});
   } else {
+    req.flash('error', 'Username and password are incorrect');
     res.redirect('/');
   }
 });
